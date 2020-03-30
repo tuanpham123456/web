d62068402fe674101d57a68f1557e1e133a05007
unknown <bicudu0403@gmail.com>
2020-03-30 18:57:56 +0700

    xử lý thêm mới route category: create, update, delete, active, hot

--- routes/route_admin.php
+++ routes/route_admin.php
@@ -5,9 +5,28 @@
             Route::get('/',function(){
                 return view ('admin.index');
             });
-    
+    // route danh mục sản phẩm
     Route::group(['prefix' => 'category'],function(){
             Route::get('','AdminCategoryController@index')->name('admin.category.index');
+            // thêm form
+            Route::get('create','AdminCategoryController@create')->name('admin.category.create');
+            // sử lý thêm mới
+            Route::post('create','AdminCategoryController@store');
+             
+            // update
+            Route::get('update{id}','AdminCategoryController@edit')->name('admin.category.update');
+            Route::post('update{id}','AdminCategoryController@update');
+
+            // xóa danh mục
+            Route::get('delete{id}','AdminCategoryController@delete')->name('admin.category.delete');
+
+            // xử lý hành động ẩn hiện danh mục
+            Route::get('active{id}','AdminCategoryController@active')->name('admin.category.active');
+
+            // xử lý sản phẩm có hot k
+            Route::get('hot{id}','AdminCategoryController@hot')->name('admin.category.hot');
+
+
     });
 });
 
