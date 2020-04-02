877a47f0a78531740dcd058abc5404105b748efe
unknown <bicudu0403@gmail.com>
2020-04-02 18:16:42 +0700

    database keyword và thư mục keyword trong admin/keyword

---
+++ database/migrations/2020_04_02_110914_create_keywords_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keywords', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('k_name');
+            $table->string('k_slug')->unique();
+            $table->string('k_description')->nullable();
+            $table->tinyInteger('k_hot')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keywords');
+    }
+}
